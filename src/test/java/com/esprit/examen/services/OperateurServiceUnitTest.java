fb3a510b345c6c5f5361d0cf7b01ea7bae12a700
chiheb97 <chiheb.benchebil@esprit.tn>
2022-10-17 23:23:09 +0100

    junit only

---
+++ src/test/java/com/esprit/examen/services/OperateurServiceUnitTest.java
@@ -0,0 +1,36 @@
+package com.esprit.examen.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.esprit.examen.entities.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OperateurServiceUnitTest {
+
+  @Autowired
+  IOperateurService opService;
+
+  @Test
+  public void TestAddOperateur() {
+    Operateur operateur = new Operateur();
+    operateur.setIdOperateur(2L);
+    operateur.setFactures(null);
+    operateur.setNom("benchebil");
+    operateur.setPassword("password");
+    operateur.setPrenom("chiheb");
+
+    Operateur op = new Operateur();
+    assertNull(op.getNom());
+    assertSame("benchebil", op.getNom());
+    opService.deleteOperateur(op.getIdOperateur());
+
+  }
+
+}

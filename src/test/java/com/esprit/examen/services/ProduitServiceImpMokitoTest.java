36447cf0f727377c00cbf8bc0a403c29aaf00fac
LaaribiGhada~ <79610455+LaaribiGhada@users.noreply.github.com>
2022-10-17 22:44:06 +0200

    addingprod test

---
+++ src/test/java/com/esprit/examen/services/ProduitServiceImpMokitoTest.java
@@ -0,0 +1,58 @@
+/*
+package com.esprit.examen.services;
+import static org.mockito.Mockito.verify;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.CategorieProduitRepository;
+import com.esprit.examen.repositories.DetailFactureRepository;
+import com.esprit.examen.repositories.ProduitRepository;
+import com.esprit.examen.repositories.StockRepository;
+
+import springfox.documentation.spring.data.rest.OptionalDeferencer;
+
+public class ProduitServiceImpMokitoTest {
+	
+	@MockBean
+    private CategorieProduitRepository categorieProduitRepository;
+
+    @MockBean
+    private ProduitRepository produitRepository;
+
+    @Autowired
+    private ProduitServiceImpl produitServiceImpl;
+
+    @MockBean
+    private StockRepository stockRepository;
+@Test
+   public void testRetreiveById(){
+	Produit p = new Produit();
+	p.setCategorieProduit(null);
+	p.setCodeProduit("123");
+	p.setDateCreation(null);
+	p.setDateDerniereModification(null);
+	p.setDetailFacture(null);
+	p.setIdProduit(1L);
+	p.setLibelleProduit("libelle");
+	p.setPrix((float)5.500);
+	p.setStock(null);
+	Optional<Produit> produits = Optional.of(p);
+	when(produitRepository.findById((Long) any())).thenReturn(produits);
+	assertSame(p,produitServiceImpl.retrieveProduit(1L));
+	verify(produitRepository).findById((Long) any());
+	
+}
+
+}
+*/
---
+++ src/test/java/com/esprit/examen/services/ProduitServiceImpTest.java
@@ -0,0 +1,51 @@
+package com.esprit.examen.services;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.validation.constraints.Null;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.ProduitRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProduitServiceImpTest {
+	
+	@MockBean
+	private ProduitRepository prodrep;
+	
+	@Autowired
+	 ProduitServiceImpl prodservImp;
+	
+	@Autowired
+	IProduitService iprodser;
+	@Autowired
+	StockServiceImpl stockservImp;
+	
+	
+	@Test
+	public void testDeleteProduit() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date dateCreation = dateFormat.parse("16/06/2022");
+	    Produit pr = new Produit("11", dateCreation);
+		Produit produit =iprodser.addProduit(pr);
+	    iprodser.deleteProduit(produit.getIdProduit());
+		assertNull(iprodser.retrieveProduit(produit.getIdProduit()));
+	}
+
+	
+	
+
+}

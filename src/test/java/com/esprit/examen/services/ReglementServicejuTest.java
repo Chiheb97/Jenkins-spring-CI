ec4cd81692502b4e379eefd9422224d57bafd3d2
AlaaEddineBoukhriss <79084849+AlaaEddineBoukhriss@users.noreply.github.com>
2022-10-18 12:41:13 +0100

    v2.4

--- src/test/java/com/esprit/examen/services/ReglementServicejuTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServicejuTest.java
@@ -1,4 +1,4 @@
-/* package com.esprit.examen.services;
+ package com.esprit.examen.services;
 
 import static org.junit.Assert.assertTrue;
 
@@ -54,4 +54,4 @@ public class ReglementServicejuTest {
 	    }
 
 }
-*/
+
